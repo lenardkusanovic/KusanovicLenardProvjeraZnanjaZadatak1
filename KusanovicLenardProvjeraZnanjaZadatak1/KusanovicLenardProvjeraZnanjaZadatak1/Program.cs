b584e14306d977e3fe85435acb17f603ba9ed09f
lenardkusanovic <55271599+lenardkusanovic@users.noreply.github.com>
2020-02-13 10:04:54 +0100

    Add files via upload

---
+++ KusanovicLenardProvjeraZnanjaZadatak1/KusanovicLenardProvjeraZnanjaZadatak1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KusanovicLenardProvjeraZnanjaZadatak1
+{
+    class Program
+    { 
+        
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 7;
+            double c = 12.5;
+            double d = 16.7;
+            double rez;
+
+            rez = (a + b + c + d) / 4;
+            Console.WriteLine("Rezultat je: " + rez);
+            Console.ReadKey(); 
+
+        }
+    }
+}
